196d46c78838a6834e09e2ee6bdfc7a3d8afc516
Daniel Mason <daniel@danielmason.com>
2020-05-24 12:33:41 +0100

    feat(heap): added heap type for min heap and max heap

---
+++ src/Heap.test.ts
@@ -0,0 +1,39 @@
+import {CompareFn} from './Compare';
+import { expect } from 'chai';
+import {Heap} from './Heap';
+
+describe('Heap', () => {
+  it('should be able to create a min heap', () => {
+    const compareFn: CompareFn<number> = (a, b) => a - b; // What you'd use to sort an array ascending
+    const minHeap = new Heap(compareFn);
+    minHeap.push(3, 9, 8, 5, 4, 2, 6, 1, 7);
+    const iter = minHeap.iter();
+    expect(iter.next().value).to.equal(1);
+    expect(iter.next().value).to.equal(2);
+    expect(iter.next().value).to.equal(3);
+    expect(iter.next().value).to.equal(4);
+    expect(iter.next().value).to.equal(5);
+    expect(iter.next().value).to.equal(6);
+    expect(iter.next().value).to.equal(7);
+    expect(iter.next().value).to.equal(8);
+    expect(iter.next().value).to.equal(9);
+    expect(iter.next().done).to.equal(true);
+  });
+
+  it('should be able to create a max heap', () => {
+    const compareFn: CompareFn<number> = (a, b) => b - a; // What you'd use to sort an array descending
+    const maxHeap = new Heap(compareFn);
+    maxHeap.push(3, 9, 8, 5, 4, 2, 6, 1, 7);
+    const iter = maxHeap.iter();
+    expect(iter.next().value).to.equal(9);
+    expect(iter.next().value).to.equal(8);
+    expect(iter.next().value).to.equal(7);
+    expect(iter.next().value).to.equal(6);
+    expect(iter.next().value).to.equal(5);
+    expect(iter.next().value).to.equal(4);
+    expect(iter.next().value).to.equal(3);
+    expect(iter.next().value).to.equal(2);
+    expect(iter.next().value).to.equal(1);
+    expect(iter.next().done).to.equal(true);
+  });
+});
---
+++ src/Heap.ts
@@ -0,0 +1,71 @@
+import {CompareFn} from './Compare';
+import {BinaryTree} from './BinaryTree';
+import {Maybe} from './Maybe';
+
+export class Heap<T> extends BinaryTree<T> {
+  constructor(protected compareFn: CompareFn<T>) {
+    super()
+  }
+
+  protected swap(i1: number, i2: number) {
+    const temp = this.tree[i1];
+    this.tree[i1] = this.tree[i2];
+    this.tree[i2] = temp;
+
+  }
+
+  protected bubbleUp(index: number) {
+    const parent = this.parentOfIndex(index);
+    if (parent !== undefined && this.compareFn(this.tree[index], this.tree[parent]) < 0) {
+      this.swap(index, parent);
+      this.bubbleUp(parent);
+    }
+  }
+
+  protected bubbleDown(index: number = 0) {
+    const swapAndBubbleDown = (newIndex: number) => {
+      this.swap(index, newIndex);
+      this.bubbleDown(newIndex);
+    }
+
+    const left = this.leftOfIndex(index);
+    const right = this.rightOfIndex(index);
+
+    const isSmallerThanLeft = left !== undefined && this.compareFn(this.tree[left], this.tree[index]) < 0;
+    const isSmallerThanRight = right !== undefined && this.compareFn(this.tree[right], this.tree[index]) < 0;
+    const isSmallerThanBoth = isSmallerThanLeft && isSmallerThanRight;
+
+    if(isSmallerThanBoth) {
+      const smaller = this.compareFn(this.tree[left!], this.tree[right!]) < 0 ? left : right;
+      return swapAndBubbleDown(smaller!);
+    }
+    if(isSmallerThanLeft) {
+      return swapAndBubbleDown(left!);
+    }
+    if(isSmallerThanRight) {
+      return swapAndBubbleDown(right!);
+    }
+  }
+
+  push(...values: Array<T>) {
+    for (const value of values) {
+      this.tree.push(value);
+      this.bubbleUp(this.tree.length - 1); // Bubble last entry
+    }
+  }
+
+  pop(): Maybe<T> {
+    if(this.tree.length > 0) {
+      this.swap(0, this.tree.length - 1);
+      const value = this.tree.pop();
+      this.bubbleDown();
+      return value;
+    }
+  }
+
+  *iter(): IterableIterator<T> {
+    while(this.tree.length > 0) {
+      yield this.pop() as T;
+    }
+  }
+}
